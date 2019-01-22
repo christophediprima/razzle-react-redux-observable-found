43512b06e2223dd524c27d50f73eb4deacda3349
Vinicius Camargo <cmrg.vinicius@gmail.com>
2019-01-22 17:36:37 +0100

    Move JSS hydrate out of createClientConfig

--- src/util/createClientConfig.ts
+++ src/util/createClientConfig.ts
@@ -27,18 +27,5 @@ export default <State = any, Action extends AnyAction = any>(
 
   if (element) {
     hydrate({ element, found, store });
-
-    /**
-     *
-     * We don't need the static css any more once we have launched our application.
-     *
-     * https://github.com/cssinjs/examples/commit/a0c178d1c99c969f8f8633c6c65b256814c455dc#diff-55465268ebcce81417d3c87474d1e9f8R6
-     *
-     */
-    const jssStyles = document.getElementById('server-side-styles');
-
-    if (jssStyles && jssStyles.parentNode) {
-      jssStyles.parentNode.removeChild(jssStyles);
-    }
   }
 };
